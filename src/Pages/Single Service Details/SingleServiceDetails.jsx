371aaf70e3c27fac0d1cc56864452353491b1575
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-11-07 20:36:30 +0600

    sent booking data to database by clicking purchase button and displayed a sweet alert after successful purchase

--- src/Pages/Single Service Details/SingleServiceDetails.jsx
+++ src/Pages/Single Service Details/SingleServiceDetails.jsx
@@ -1,8 +1,11 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
-
+import { AuthContext } from "../../Providers/AuthProvider";
+import Swal from 'sweetalert2';
 const SingleServiceDetails = () => {
     const [singleService, setSingleService] = useState({})
+
+    const { user } = useContext(AuthContext);
 
     const singleServiceId = useParams();
 
@@ -12,6 +15,35 @@ const SingleServiceDetails = () => {
         const singleService = singleServiceData.find(service => service._id == singleServiceId.id)
         setSingleService(singleService);
     }, [singleServiceData, singleServiceId.id])
+
+    const handleBoookings = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const serviceName = form.serviceName.value;
+        const userEmail = form.userEmail.value;
+        const bookingDate = form.bookingDate.value;
+        const userAddress = form.userAddress.value;
+
+        const bookings = { serviceName, userEmail, bookingDate, userAddress }
+
+        fetch('http://localhost:5000/bookings', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(bookings)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: "You purchased this service!",
+                        text: "Check your My Schedules page to see your purchase",
+                        icon: "success"
+                    });
+                }
+            })
+    }
 
     return (
         <div>
@@ -29,7 +61,36 @@ const SingleServiceDetails = () => {
                     <p className='font-semibold'>Provider: {singleService.service_provider_name}</p>
                     <p className='font-semibold'>Price: {singleService.service_price}</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-success normal-case">Book Now</button>
+                        <button className="btn btn-success normal-case" onClick={() => document.getElementById('my_modal_5').showModal()}>Book Now</button>
+                        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                            <div className="modal-box">
+                                <form onSubmit={handleBoookings}>
+                                    <div>
+                                        <input className="font-bold text-lg" type="text" name="serviceName" defaultValue={singleService.service_name} readOnly />
+                                    </div>
+                                    <div>
+                                        <span className="font-semibold">Your Email: </span>
+                                        <input className="py-4 font-semibold" type="email" name="userEmail" defaultValue={user && user.email} readOnly />
+                                    </div>
+                                    <div className="py-4">
+                                        <span className="font-semibold">Booking Date: </span>
+                                        <input type="date" name="bookingDate" id="" required />
+                                    </div>
+                                    <div className="py-4">
+                                        <span className="font-semibold">Your Address: </span>
+                                        <input className="border-2 border-black rounded" type="text" name="userAddress" id="" required />
+                                    </div>
+                                    <div className="text-center pt-10">
+                                        <button className="btn btn-success normal-case">Purchase</button>
+                                    </div>
+                                </form>
+                                <div className="modal-action">
+                                    <form method="dialog">
+                                        <button className="btn btn-success normal-case">Close</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </dialog>
                     </div>
                 </div>
             </div>
